dbc6b639b9355484ac437e165c6c8e119b94ab91
pravakar <paltasingh10@gmail.com>
2025-02-07 02:00:46 +0530

    Initial Commits - K-mean Clustering

--- k-mean-clustering.py
+++ k-mean-clustering.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from utils import *
+from public_tests import *
 
 def find_closet_centroids(X, centroids):
     """
@@ -33,9 +34,38 @@ print(f"First five elements of X are: \n{X[:5]}")
 print(f"The shape of X is:", X.shape)
 
 initial_centroids = np.array([[3,3], [6,2], [8,5]])
-idx = find_closest_centroids(X, initial_centroids)
+idx = find_closet_centroids(X, initial_centroids)
 
 print(f"First three elements in idx are:", idx[:3])
 
-from public_tests import *
-find_closest_centroids_test(find_closest_centroids)
+find_closest_centroids_test(find_closet_centroids)
+
+# Computing cetroid means
+def compute_centroids(X, idx, K):
+    """Returns the new centroids by computing the means of the data points assigned to each centroid.
+
+    Args:
+        X (ndarray): (m, n) Data points
+        idx (ndarray): (m, ) Array containing index of closest centroid for each
+        example in X. concretely, idx[i] contains the index of
+        the centroid closest to example i
+        K (int): number of centroids
+        
+    Returns:
+        centroids (ndarray): (K, n) New centroids computed
+    """
+    
+    m, n = X.shape
+    centroids = np.zeros((K, n))
+    
+    for k in range(K):
+        points = X[idx == k]
+        centroids[k] = np.mean(points, axis = 0)
+        
+    return centroids
+
+K = 3
+centroids = compute_centroids(X, idx, K)
+print(f"The centroids are: {centroids}")
+
+compute_centroids_test(compute_centroids)
